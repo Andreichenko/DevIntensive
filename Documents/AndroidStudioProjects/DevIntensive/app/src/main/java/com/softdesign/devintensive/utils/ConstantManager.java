34d25cca3456494ce261d864f81de55d0daa4b71
AlexFrei <rsn_alex@mail.ru>
2016-06-24 00:36:06 +0300

    new 2

---
+++ Documents/AndroidStudioProjects/DevIntensive/app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
@@ -0,0 +1,9 @@
+package com.softdesign.devintensive.utils;
+
+/**
+ * Created by AlexFrei on 22.06.16.
+ */
+public interface ConstantManager {
+    String TAG_PREFIX="DEV";
+    String COLOR_MODE_KEY="COLOR_MODE_KEY";
+}
