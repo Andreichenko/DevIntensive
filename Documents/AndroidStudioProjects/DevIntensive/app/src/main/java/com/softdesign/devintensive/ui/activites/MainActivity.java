c13e137bf29c8d290d0937f8d17db529c6790380
AlexFrei <rsn_alex@mail.ru>
2016-06-25 23:26:08 +0300

    complited 2 переделал скролл начинаю делать 3 задание

--- Documents/AndroidStudioProjects/DevIntensive/app/src/main/java/com/softdesign/devintensive/ui/activites/MainActivity.java
+++ Documents/AndroidStudioProjects/DevIntensive/app/src/main/java/com/softdesign/devintensive/ui/activites/MainActivity.java
@@ -1,6 +1,7 @@
 package com.softdesign.devintensive.ui.activites;
 
 import android.graphics.Color;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,17 +9,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final String TAG = ConstantManager.TAG_PREFIX+"Main Activity";
+    private static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
 
-    /* protected EditText mEditText;
-    protected Button mRedButton, mGreenButton;
-    protected int mColorMode;
-    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mGreenButton.setOnClickListener(this);
         */
 
-        if (savedInstanceState == null){
+        if (savedInstanceState == null) {
 
         } else {
 
@@ -49,49 +49,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             */
 
         }
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.d(TAG, "onStart");
+
     }
+        @Override
+        protected void onResume() {
+            super.onResume();
+            Log.d(TAG, "onResume");
+        }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume");
-    }
+        @Override
+        protected void onPause() {
+            super.onPause();
+            Log.d(TAG, "onPause");
+        }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause");
-    }
+        @Override
+        protected void onStop() {
+            super.onStop();
+            Log.d(TAG, "onStop");
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop");
-    }
+        }
+        @Override
+        protected void onDestroy () {
+            super.onDestroy();
+            Log.d(TAG, "onDestroy");
+        }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG, "onRestart");
-    }
-
+        @Override
+        protected void onRestart () {
+            super.onRestart();
+            Log.d(TAG, "onRestart");
+        }
 
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
+        @Override
+        public void onClick (View v){
+            switch (v.getId()) {
            /* case R.id.green_btn:
                 mEditText.setBackgroundColor(Color.GREEN);
                 mColorMode = Color.GREEN;
@@ -101,17 +101,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 mColorMode = Color.RED;
                 break;
                 */
+            }
         }
-    }
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+
+        @Override
+        protected void onSaveInstanceState(Bundle outState){
+            super.onSaveInstanceState(outState);
 
        /* Log.d(TAG, "onSaveInstanceState");
-        outState.putInt(ConstantManager.COLOR_MODE_KEY, mColorMode);
+        outState.putInt(ConstantManager.COLOR_MODE_KEY, mColorMode); */
+        }
 
-        /Applications/Android Studio.app/Contents/plugins/android/lib/templates/gradle-projects/
-        NewAndroidModule/root/res/mipmap-xxxhdpi/ic_launcher.png
-        */
-    }
 }
