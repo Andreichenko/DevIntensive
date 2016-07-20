ad1e94c49e6787fff924ca8ae9eb47aa209916d8
AlexFrei <rsn_alex@mail.ru>
2016-07-20 10:04:10 +0300

    Hometask_7 Домашнее задание 6 сделано и откомплировано оценка 0

--- Documents/AndroidStudioProjects/DevIntensive/app/src/main/java/com/softdesign/devintensive/ui/activites/AuthActivity.java
+++ Documents/AndroidStudioProjects/DevIntensive/app/src/main/java/com/softdesign/devintensive/ui/activites/AuthActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
